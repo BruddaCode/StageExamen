71bb02ddcedc553e07c0b06e5dfa808df7152b37
Yasin Coban <cobanyasin234@gmail.com>
2024-05-14 16:39:34 +0200

    Push scanner page

--- SnapCook/public/footer.php
+++ SnapCook/public/footer.php
@@ -72,7 +72,7 @@
           <div class="flex mt-4 sm:justify-center sm:mt-0">
               <a href="#" class="text-gray-900 hover:text-gray-900 dark:hover:text-white">
                   <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
-                        <path fill-rule="evenodd" d="M22.675 0h-21.35c-.732 0-1.325.593-1.325 1.325v21.351c0 .731.593 1.324 1.325 1.324h11.495v-9.294h-3.128v-3.622h3.128v-2.671c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12v9.293h6.116c.73 0 1.323-.593 1.323-1.325v-21.35c0-.732-.593-1.325-1.325-1.325z" clip-rule="evenodd"/>
+                        <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
                     </svg>
                   <span class="sr-only">Facebook page</span>
               </a>
--- SnapCook/public/script.js
+++ SnapCook/public/script.js
@@ -10,4 +10,10 @@ document.addEventListener('DOMContentLoaded', function () {
     
   });
 
+  document.getElementById('imageButton').addEventListener('click', function() {
+    // Code to handle image insertion or dropping
+    // This could involve opening a file dialog or interacting with a file drop area
+    // Once the image is selected or dropped, you can insert it into the desired location
+  });
+
   
