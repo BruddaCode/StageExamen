09f82ab32f1de9ef3cf1fb25a7fe297fed9b6d3a
Yasin Coban <cobanyasin234@gmail.com>
2024-05-14 11:52:15 +0200

    footer en nav kleur veranderd

--- SnapCook/public/footer.php
+++ SnapCook/public/footer.php
@@ -13,7 +13,7 @@
 
 
 
-<footer style="background-color: #D9D9D9;" class="dark:bg-gray-900">
+<footer style="background-color: #F3F2F2;" class="dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div class="md:flex md:justify-between">
           <div class="mb-6 md:mb-0">
